b5a933f5755c06cb83c5f71bb7acd5c4cdbdaed7
xodus05 <s2121@e-mirim.hs.kr>
2022-02-02 18:05:33 +0900

    라이브러리 수정, 메인코드

--- app/src/main/java/com/example/calendartest/MainActivity.java
+++ app/src/main/java/com/example/calendartest/MainActivity.java
@@ -1,14 +1,72 @@
 package com.example.calendartest;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import androidx.annotation.IdRes;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.OrientationHelper;
+import androidx.appcompat.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.RadioGroup;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.applikeysolutions.cosmocalendar.utils.SelectionType;
+import com.applikeysolutions.cosmocalendar.view.CalendarView;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements
+RadioGroup.OnCheckedChangeListener {
+
+        private CalendarView calendarView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setSupportActionBar((Toolbar) findViewById(R.id.toolbar));
+
+        initViews();
+    }
+
+    private void initViews() {
+        calendarView = (CalendarView) findViewById(R.id.calendar_view);
+        calendarView.setCalendarOrientation(OrientationHelper.HORIZONTAL);
+
+        ((RadioGroup) findViewById(R.id.rg_selection_type)).setOnCheckedChangeListener(this);
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+
+            case R.id.clear_selections:
+                clearSelectionsMenuClick();
+                return true;
+
+            case R.id.show_selections:
+                List<Calendar> days = calendarView.getSelectedDates();
+
+                String result="";
+                for (int i = 0; i<days.size(); i++)
+                {
+                    Calendar calendar = days.get(i);
+                    final int day = calendar.get(Calendar.DAY_OF_MONTH);
+                }
+        }
+    }
+
+    private void clearSelectionsMenuClick() {
     }
 }
